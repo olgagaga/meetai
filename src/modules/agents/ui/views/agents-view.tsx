7b5ac422289924dcf7be870697b6fc45300cf19e
olgagaga <olga_kuzmich2005@tut.by>
2025-09-20 17:36:14 -0400

    10: responsive dialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -2,8 +2,7 @@
 
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
-// import { LoadingState } from "@/components/loading-state";
-// import { ErrorState } from "@/components/error-state";
+
 
 export const AgentsView = () => {
   const trpc = useTRPC();
@@ -11,7 +10,6 @@ export const AgentsView = () => {
 
   return (
     <>
-      <div><p>Hisf gsgid</p></div>
       <div>{JSON.stringify(data, null, 2)}</div>
     </>
   );
