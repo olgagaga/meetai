1a646a947e77fdd40883ca06864342cd99b05d0b
olgagaga <olga_kuzmich2005@tut.by>
2025-09-25 10:12:18 -0400

    11: agents form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/client";
 
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
   variable: "--font-geist-sans",
@@ -23,7 +24,8 @@ export default function RootLayout({
   return (
     <TRPCReactProvider>
       <html lang="en">
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <body className={`${inter.className} antialiased`}>
+          <Toaster/>{children}</body>
       </html>
     </TRPCReactProvider>
   );
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,13 +1,25 @@
-import {initTRPC} from "@trpc/server";
-import {cache} from "react";
+import { initTRPC } from "@trpc/server";
+import { cache } from "react";
 export const createTRPCContext = cache(async () => {
-    return {userId: "user_123"};
+  return { userId: "user_123" };
 });
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { TRPCError } from "@trpc/server";
 
-const t = initTRPC.create({
-
-});
+const t = initTRPC.create({});
 
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "Unathorized" });
+  }
+
+  return next({ ctx: { ...ctx, auth: session } });
+});
